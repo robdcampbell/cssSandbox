cd0d74f149c539466184e23bef40211b4d750b45
Rob Campbell <rdeancampbell@gmail.com>
2019-06-20 22:19:22 -0400

    finish initial bubbleNav, begin refactoring

--- 04_StickyNav/main.js
+++ 04_StickyNav/main.js
@@ -1,1 +1,12 @@
-console.log('test');
+// console.log('test');
+
+const list = document.querySelector('ul');
+const bubble = document.querySelector('.bubble');
+
+list.addEventListener('click', (e)=> {
+  const target = e.target;
+  if(target.tagName === 'A'){
+    const { width, left} = target.getBoundingClientRect();
+    bubble.style.cssText = `width: ${width}px; left: ${left}px;`
+  }
+})
